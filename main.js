0c2e5ef3d293bc208831770ac2179859e26b64c7
David Winter <i@djw.me>
2011-10-30 22:49:24 +0000

    Refactored tweet get logic and looping.
    Now using callback to add markers for each tweet.

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+function get_tweets(latitude, longitude, tweet_loop) {
+	$.getJSON('http://search.twitter.com/search.json?geocode='+latitude+','+longitude+',1km&result_type=recent&rpp=100&callback=?', function(data) {
+		if (data.results) {
+			for (i = 0; i < data.results.length; i++) {  
+				if (data.results[i].geo) {
+					tweet_loop(data.results[i]);
+				}
+			}				
+		}
+	});
+}
+
+function tweet_popup(tweet) {
+	return '<div class="tweet_content"><img src="'+tweet.profile_image_url+'" alt="" /><blockquote>'+tweet.text+'</blockquote><div class="tweet_user"><a href="https://twitter.com/'+tweet.from_user+'" class="twitter-follow-button" data-show-count="false">Follow @'+tweet.from_user+'</a></div></div>';
+}
+
 $(function() {
 	
 	navigator.geolocation.getCurrentPosition(show_leafletmap);
@@ -10,38 +26,23 @@ $(function() {
 		var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/ce3741232bc240ad97bdba9bc6bde548/997/256/{z}/{x}/{y}.png',
 		    cloudmade = new L.TileLayer(cloudmadeUrl, { maxZoom: 18 });
 		
-		var london = new L.LatLng(position.coords.latitude, position.coords.longitude); // geographical point (longitude and latitude)
-		map.setView(london, 15).addLayer(cloudmade);
+		var current_location = new L.LatLng(position.coords.latitude, position.coords.longitude);
+		map.setView(current_location, 15).addLayer(cloudmade);
 
-		var marker = new L.Marker(new L.LatLng(position.coords.latitude, position.coords.longitude));
-		map.addLayer(marker);
-		marker.bindPopup('You!').openPopup();
-
-		/** Twitter **/
-
-		$.getJSON('http://search.twitter.com/search.json?geocode='+position.coords.latitude+','+position.coords.longitude+',1km&result_type=recent&rpp=100&callback=?', function(data) {
-			if (data.results) {
-				for (i = 0; i < data.results.length; i++) {  
-  					if (data.results[i].geo) {
-						marker = new L.Marker(new L.LatLng(data.results[i].geo.coordinates[0], data.results[i].geo.coordinates[1]));
-						map.addLayer(marker);
-						marker.bindPopup('<div class="tweet_content"><img src="'+data.results[i].profile_image_url+'" alt="" /><blockquote>'+data.results[i].text+'</blockquote><div class="tweet_user"><a href="https://twitter.com/'+data.results[i].from_user+'" class="twitter-follow-button" data-show-count="false">Follow @'+data.results[i].from_user+'</a></div></div>');
-						marker.on('click', function(e) {
-							twttr.widgets.load();
-						});
-					}
-				}				
-			}
+		get_tweets(position.coords.latitude, position.coords.longitude, function(tweet) {
+			marker = new L.Marker(new L.LatLng(tweet.geo.coordinates[0], tweet.geo.coordinates[1]));
+			map.addLayer(marker);
+			marker.bindPopup(tweet_popup(tweet));
+			marker.on('click', function(e) {
+				twttr.widgets.load();
+			});
 		});
 	}
 
 	function show_gmap(position) {
-		var latitude = position.coords.latitude;
-		var longitude = position.coords.longitude;
-
 		map_options = {
 			zoom: 15,
-			center: new google.maps.LatLng(latitude,longitude),
+			center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 
@@ -52,29 +53,22 @@ $(function() {
 
 		infowindow = new google.maps.InfoWindow();
 
-		$.getJSON('http://search.twitter.com/search.json?geocode='+latitude+','+longitude+',1km&result_type=recent&rpp=100&callback=?', function(data) {
-			if (data.results) {
+		get_tweets(position.coords.latitude, position.coords.longitude, function(tweet) {
+			marker = new google.maps.Marker({
+				position: new google.maps.LatLng(
+						tweet.geo.coordinates[0], 
+						tweet.geo.coordinates[1]
+					),
+				map: map
+			});
 
-				for (i = 0; i < data.results.length; i++) {  
-  					if (data.results[i].geo) {
-						marker = new google.maps.Marker({
-							position: new google.maps.LatLng(
-									data.results[i].geo.coordinates[0], 
-									data.results[i].geo.coordinates[1]
-								),
-							map: map
-						});
-
-						google.maps.event.addListener(marker, 'click', (function(marker, i) {
-							return function() {
-								infowindow.setContent('<div class="tweet_content"><img src="'+data.results[i].profile_image_url+'" alt="" /><blockquote>'+data.results[i].text+'</blockquote><div class="tweet_user"><a href="https://twitter.com/'+data.results[i].from_user+'" class="twitter-follow-button" data-show-count="false">Follow @'+data.results[i].from_user+'</a></div></div>');
-								infowindow.open(map, marker);
-								twttr.widgets.load();
-							}
-						})(marker, i));
-					}
-				}				
-			}
+			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+				return function() {
+					infowindow.setContent(tweet_popup(tweet));
+					infowindow.open(map, marker);
+					twttr.widgets.load();
+				}
+			})(marker, i));
 		});
 	}
 
